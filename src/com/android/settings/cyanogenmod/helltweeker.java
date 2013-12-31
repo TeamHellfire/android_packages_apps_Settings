c00293f7e71c683ca25fab37aeeb325a3cfec408
Ayysir <dresadd09691@gmail.com>
2013-12-31 16:44:57 -0500

    HellfireTweaker: add modified copyright

--- src/com/android/settings/cyanogenmod/helltweeker.java
+++ src/com/android/settings/cyanogenmod/helltweeker.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012 The Android Open Source Project
+ * This code has been modified. Portions copyright (C) 2014, HellFire Project.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
